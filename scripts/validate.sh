948aaeaa5a07b3b39a79cc4d51cea32d8ba8682f
Vlad <vlad@ghn.me>
2015-06-03 11:13:09 -0500

    Add validate script

---
+++ scripts/validate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Load common functions
+. $(dirname $0)/common.sh
+
+# Check Docker
+is_cmd docker || e_abort 'Docker is not installed!'
+# Check Docker Compose
+is_cmd docker-compose || e_abort 'Docker compose is not installed!'
+
+# Wait for web server to start
+wait_for 'curl --insecure --output /dev/null --silent --head --fail https://localhost:8140/puppet-ca/v1/certificate/ca'
+
+# Check running containers
+container_running \
+  aws_PuppetServer_1
+
+e_finish
