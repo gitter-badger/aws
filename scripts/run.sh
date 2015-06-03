8985118646cf05226bf186e582c72e208208b689
Vlad <vlad@ghn.me>
2015-06-03 11:27:49 -0500

    Fix SUDO

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,8 +7,8 @@
 if [ -s $COMPOSE ] && [ -x $COMPOSE ]; then
   echo "'${COMPOSE}' is present."
 else
-  curl -Lo $COMPOSE https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m`
-  chmod +x $COMPOSE
+  sudo curl -Lo $COMPOSE https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m`
+  sudo chmod +x $COMPOSE
 fi
 
 # Pull all images
