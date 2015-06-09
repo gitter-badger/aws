798aaa534b4c0d7a22a9d353d6d24d8c65062920
Vlad <vlad@ghn.me>
2015-06-09 08:48:13 -0500

    Improve pre script

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,18 +4,29 @@
 . $(dirname $0)/common.sh
 
 # Update APT
-sudo apt-get -qqy update
+sudo apt-get -y update
 
-# Install docker-compose if not installed already
+# Ensure Docker is installed
+if is_cmd docker && [ -e /var/run/docker.sock ]; then
+  echo 'Docker Version:'
+  ${DOCKER} version
+else
+  wget -qO- https://get.docker.com/ | sh
+fi
+
+# Ensure Docker Compose is installed
+COMPOSE_VERSION='1.2.0'
+COMPOSE_URL='https://github.com/docker/compose/releases/download'
+COMPOSE_SCRIPT="docker-compose-$(uname -s)-$(uname -m)"
 if [ -s $COMPOSE ] && [ -x $COMPOSE ]; then
   echo "'${COMPOSE}' is present."
 else
-  sudo curl -Lo $COMPOSE https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m`
+  sudo curl -Lo $COMPOSE ${COMPOSE_URL}/${COMPOSE_VERSION}/${COMPOSE_SCRIPT}
   sudo chmod +x $COMPOSE
 fi
 
-# Install AWS CLI if not installed already
-is_cmd pip || sudo apt-get install -qqy python-pip
+# Ensure AWS CLI is installed
+is_cmd pip || sudo apt-get install -y python-pip
 is_cmd aws && aws --version || sudo pip install awscli
 
 # DONE
