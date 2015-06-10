247cf84aed266ad30d5288d8d6d98b2d0be6fe32
Vlad <vlad@ghn.me>
2015-06-10 16:11:27 -0500

    Revert to manual deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,19 @@
 # Load Functions
 . <(wget -qO- https://vladgh.s3.amazonaws.com/scripts/common.sh) || true
 
+sha=$(git rev-parse --short HEAD)
+branch=$(git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3)
+[[ $branch == 'master' ]] && branch='production'
+cd_key="${branch}/${cd_key}-${sha}.${cd_bundle}"
+
 # Deploy
 echo 'Deploying...'
+aws deploy push \
+  --application-name ${cd_app} \
+  --ignore-hidden-files \
+  --s3-location s3://${cd_bucket}/${key} \
+  --source ./
+aws deploy create-deployment --application-name ${cd_app} --s3-location bucket=${cd_bucket},key=${key},bundleType=${cd_bundle} --deployment-group-name ${cd_group} --deployment-config-name ${cd_config}
 
 # DONE
 e_finish
