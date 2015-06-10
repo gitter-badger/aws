a1fb760fcc5dc8d1b9091e52485354f904b0a6ad
Vlad <vlad@ghn.me>
2015-06-10 16:29:56 -0500

    Import remote was functions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 # Load Functions
 . <(wget -qO- https://vladgh.s3.amazonaws.com/scripts/common.sh) || true
+source_remote_script aws.sh
 
 sha=$(git rev-parse --short HEAD)
 branch=$(git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3)
@@ -10,12 +11,12 @@ cd_key="${branch}/${cd_key}-${sha}.${cd_bundle}"
 
 # Deploy
 echo 'Deploying...'
-aws deploy push \
+awscmd deploy push \
   --application-name ${cd_app} \
   --ignore-hidden-files \
   --s3-location s3://${cd_bucket}/${cd_key} \
   --source ./
-aws deploy create-deployment --application-name ${cd_app} --s3-location bucket=${cd_bucket},key=${cd_key},bundleType=${cd_bundle} --deployment-group-name ${cd_group} --deployment-config-name ${cd_config}
+awscmd deploy create-deployment --application-name ${cd_app} --s3-location bucket=${cd_bucket},key=${cd_key},bundleType=${cd_bundle} --deployment-group-name ${cd_group} --deployment-config-name ${cd_config}
 
 # DONE
 e_finish
