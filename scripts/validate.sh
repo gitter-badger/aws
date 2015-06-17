627a957415433cff3323134b347c5c9ad72cccc5
Vlad <vlad@ghn.me>
2015-06-17 18:47:30 -0500

    Document

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -3,17 +3,17 @@
 # Load Functions
 . $(dirname $0)/common.sh
 
+## System Checks
 # Check Docker
 is_cmd docker || e_abort 'Docker is not installed!'
 # Check Docker Compose
 is_cmd docker-compose || e_abort 'Docker compose is not installed!'
 
+## PuppetServer Checks
 # Wait for PuppetServer to start and return certificate
 wait_for 'curl --insecure --output /dev/null --silent --fail https://localhost:8140/puppet-ca/v1/certificate/ca'
-
 # Check running containers
-container_is_running \
-  vgh_puppetserver_1
+container_is_running vgh_puppetserver_1
 
 # DONE
 e_finish
